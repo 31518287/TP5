a9f9edf3b8434779c62420ffbeb94b69ee05c00b
31518287 <williamsebastianbrito@hotmail.com>
2024-04-12 20:52:01 -0300

    V1.2
    JJAJA

--- src/main/java/lp/tp5/TP5.java
+++ src/main/java/lp/tp5/TP5.java
@@ -1,13 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
- */
-package lp.tp5;
 
-/**
- *
- * @author rodrigo
- */
+package lp.tp5;
+//william brito xdxdxdxdxdxdxd
+
 public class TP5 extends javax.swing.JFrame {
 
     /**
